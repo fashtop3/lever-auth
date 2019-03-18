0ab00f01a8624c6f9cc06c0f5d3a4ccc7b9bc76d
Fashola Ayodeji <fashtop3@gmail.com>
2019-03-18 14:42:57 +0100

    authentication fixes

--- lever_jwt/utils.py
+++ lever_jwt/utils.py
@@ -1,10 +1,11 @@
+import json
 from functools import wraps
 from flask import request, jsonify
 
 from .lever_jwt import jwt_decode_auth_token
 
 
-def authenticate(f):
+def authenticate(f, session=None):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         response_object = {
@@ -13,12 +14,18 @@ def authenticate(f):
         }
         auth_header = request.headers.get('Authorization')
         if not auth_header:
-            return jsonify(response_object), 403
+            return jsonify(response_object), 401
         auth_token = auth_header.split(" ")[1]
         resp = jwt_decode_auth_token(auth_token)
         if isinstance(resp, str):
             response_object['message'] = resp
-            return jsonify(response_object), 401
+            return jsonify(response_object), 403
+        if session:
+            session_data = json.loads(session.get(resp.get('id')))
+            if session_data is None:
+                response_object["message"] = "Authentication invalid"
+                return jsonify(response_object), 401
+            kwargs.update(dict(session_data=session_data))
         kwargs.update(dict(resp=resp))
         return f(*args, **kwargs)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lever-jwt',
-    version='0.1.2',
+    version='0.1.3',
     description='lever jwt implementation',
     license='MIT',
     packages=['lever_jwt'],
