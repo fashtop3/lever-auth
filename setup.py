01bec43b2ad626fb1530a0fb7462e9e9fcc73c26
Fashola Ayodeji <fashtop3@gmail.com>
2019-03-19 10:28:02 +0100

    fixes for 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lever-jwt',
-    version='0.1.7',
+    version='0.1.8',
     description='lever jwt implementation',
     license='MIT',
     packages=['lever_jwt'],
